a24bbc155b78c8999971340a1926fd693217bc1f
Richard Macias <RJNY86@gmail.com>
2015-03-24 21:45:15 -0400

    string length validations and truncate index titles

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,7 +6,8 @@ class Recipe < ActiveRecord::Base
   accepts_nested_attributes_for :ingredients, :reject_if => :all_blank, :allow_destroy => true
   accepts_nested_attributes_for :directions, :reject_if => :all_blank, :allow_destroy => true
 
-  validates :title, :description, :image, presence: true
+  validates :title, :description, :image, :ingredients, :directions, presence: true
+  validates :title, :description, length: { in: 3..140 }
   has_attached_file :image, styles: { medium: "400x400#" }, default_url: "/images/:style/missing.png", preserve_files: true
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
